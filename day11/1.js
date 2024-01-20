73cd809629d1bbce9a7ea062602c7d02ae94ffa4
Tales Ruan <talesruan@gmail.com>
2024-01-20 01:00:59 -0300

    Refactor.

--- day11/1.js
+++ day11/1.js
@@ -2,14 +2,14 @@ const fs = require("fs");
 // const input = fs.readFileSync("demoinput.txt", "utf8");
 const input = fs.readFileSync("input.txt", "utf8");
 
+const UNIVERSE_EXPANSION = 2;
 
 const fn = input => {
 	const data = parseInput(input);
-	const grid = new Grid(data);
-	display(grid);
-	const galaxies = findGalaxies(grid);
-	const galaxiesPerRow = "0".repeat(grid.sizeY).split("").map(x => parseInt(x));
-	const galaxiesPerCol = "0".repeat(grid.sizeX).split("").map(x => parseInt(x));
+	const numberOfLines = data.length;
+	const galaxies = findGalaxies(data);
+	const galaxiesPerRow = "0".repeat(numberOfLines).split("").map(x => parseInt(x));
+	const galaxiesPerCol = "0".repeat(data[0].length).split("").map(x => parseInt(x));
 	for (const galaxy of galaxies) {
 		galaxiesPerRow[galaxy.y]++;
 		galaxiesPerCol[galaxy.x]++;
@@ -21,12 +21,18 @@ const fn = input => {
 		const galaxy2 = galaxyPair[1];
 		let steps = 0;
 		for (let i = galaxy1.x; i !== galaxy2.x ; i += (Math.sign(galaxy2.x - galaxy1.x))) {
-			steps ++;
-			if (galaxiesPerCol[i] === 0) steps++;
+			if (galaxiesPerCol[i] === 0) {
+				steps += UNIVERSE_EXPANSION;
+			} else {
+				steps ++;
+			}
 		}
 		for (let i = galaxy1.y; i !== galaxy2.y ; i += (Math.sign(galaxy2.y - galaxy1.y))) {
-			steps ++;
-			if (galaxiesPerRow[i] === 0) steps++;
+			if (galaxiesPerRow[i] === 0) {
+				steps += UNIVERSE_EXPANSION;
+			} else {
+				steps ++;
+			}
 		}
 		sumOfSteps += steps;
 	}
@@ -43,68 +49,17 @@ const getPairs = array => {
 	return pairs;
 }
 
-const findGalaxies = grid => {
+const findGalaxies = data => {
 	const galaxies = [];
-	grid.iterate((x, y, value) => {
-		if (value === "#") galaxies.push({x, y});
-	});
-	return galaxies;
-};
-
-class Grid {
-	matrix;
-	sizeY = 0;
-	sizeX = 0;
-
-	constructor (matrix = []) {
-		this.matrix = matrix;
-		this.sizeY = matrix.length;
-		if (this.matrix.length > 0) this.sizeX = this.matrix[0].length;
-	}
-
-	iterate (func) {
-		for (let y = 0; y < this.sizeY; y++) {
-			for (let x = 0; x < this.sizeX; x++) {
-				const value = this.get(x, y);
-				func(x, y, value);
-			}
+	for (let y = 0; y < data.length; y++) {
+		const line = data[y];
+		for (let x = 0; x < line.length; x++) {
+			const value = line[x];
+			if (value === "#") galaxies.push({x, y});
 		}
 	}
-
-	getAdjacentValues (x, y) {
-		return this.getAdjacentCoords(x, y).map(coord => this.get(coord.x, coord.y));
-	}
-
-	getAdjacentCoords (x, y) { // TODO: Remove
-		return [
-			// {x: x - 1, y: y - 1},
-			{x: x + 0, y: y - 1},
-			// {x: x + 1, y: y - 1},
-			{x: x - 1, y: y + 0},
-			//{x: x + 0, y: y + 0},
-			{x: x + 1, y: y + 0},
-			// {x: x - 1, y: y + 1},
-			{x: x + 0, y: y + 1},
-			// {x: x + 1, y: y + 1},
-		].filter(coord => this.get(coord.x, coord.y) !== undefined);
-	}
-
-	get (x, y) {
-		if (!this.matrix[y]) return;
-		return this.matrix[y][x];
-	}
-
-	set (x, y, value) {
-		if (x > this.sizeX - 1) this.sizeX = x + 1;
-		if (y > this.sizeY - 1) this.sizeY = y + 1;
-		if (!this.matrix[y]) this.matrix[y] = [];
-		this.matrix[y][x] = value;
-	}
-
-	isOutside (x, y) {
-		return x < 0 || y < 0 || x >= this.sizeX || y >= this.sizeY;
-	}
-}
+	return galaxies;
+};
 
 const parseInput = input => {
 	const data = [];
@@ -112,27 +67,6 @@ const parseInput = input => {
 		data.push(line.split(""));
 	}
 	return data;
-};
-
-const display = (grid) => {
-	for (let y = 0; y < grid.matrix.length; y++) {
-		let line = "";
-		for (let x = 0; x < grid.sizeX; x++) {
-			let value = grid.get(x, y);
-			if (value === "X") {
-				value = getColoredString(value, "red");
-			} else if (value === "E") {
-				value = getColoredString(value, "green");
-			} else if (value === "O") {
-				value = getColoredString(value, "green");
-			}
-			let v = " " + value;
-			if (value === undefined) v = "  ";
-
-			line += v;
-		}
-		console.log(line);
-	}
 };
 
 console.time("Elapsed time");
